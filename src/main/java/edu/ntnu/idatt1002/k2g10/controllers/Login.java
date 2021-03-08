45486317ea6ab36d084776ac91be08a00925a165
Tobias Rødahl Thingnes <tobiasth@stud.ntnu.no>
2021-03-08 09:38:22 +0000

    feat(themes): Add themes support

---
+++ src/main/java/edu/ntnu/idatt1002/k2g10/controllers/Login.java
@@ -0,0 +1,37 @@
+package edu.ntnu.idatt1002.k2g10.controllers;
+
+import com.jfoenix.controls.JFXPasswordField;
+import com.jfoenix.controls.JFXTextField;
+import javafx.scene.input.KeyCode;
+import javafx.scene.input.KeyEvent;
+
+public class Login {
+  public JFXTextField usernameField;
+  public JFXPasswordField passwordField;
+
+  /**
+   * Event handler for clicks on the "login" button.
+   *
+   * @author trthingnes
+   */
+  public void buttonClickLogin() {
+    System.out.println("Login clicked.");
+    System.out.printf(
+        "Got username '%s' and password '%s'.%n", usernameField.getText(), passwordField.getText());
+  }
+
+  /**
+   * Event handler for key presses on the "login" button and frame.
+   *
+   * @param event Event information
+   * @author trthingnes
+   */
+  public void keyPressLogin(KeyEvent event) {
+    if (event.getCode() == KeyCode.ENTER) {
+      System.out.println("Enter pressed.");
+      System.out.printf(
+          "Got username '%s' and password '%s'.%n",
+          usernameField.getText(), passwordField.getText());
+    }
+  }
+}
--- src/test/java/TodoListAppTest.java
+++
@@ -1,1 +0,0 @@
-public class TodoListAppTest {}
